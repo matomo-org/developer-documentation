c437dfd43e72a7258fbb190ec554cd8feef71cd5
Thomas Steur <thomas.steur@gmail.com>
2013-11-05 01:54:41 +0000

    added possibility to define a call to action and added more space after call to action

--- app/helpers/Menu.php
+++ app/helpers/Menu.php
@@ -20,21 +20,24 @@ class Menu {
         $menu = array();
 
         $menu['guides'] = array(
-            'title'       => 'Guides',
-            'url'         => static::getUrl('guides'),
-            'description' => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+            'title'        => 'Guides',
+            'url'          => static::getUrl('guides'),
+            'description'  => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+            'callToAction' => 'Get started'
         );
 
         $menu['api-reference'] = array(
-            'title'       => 'API Reference',
-            'url'         => static::getUrl('api-reference'),
-            'description' => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+            'title'        => 'API Reference',
+            'url'          => static::getUrl('api-reference'),
+            'description'  => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+            'callToAction' => 'View Details'
         );
 
         $menu['support'] = array(
-            'title'       => 'Support',
-            'url'         => static::getUrl('support'),
-            'description' => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+            'title'        => 'Support',
+            'url'          => static::getUrl('support'),
+            'description'  => 'Extend Piwik by writing your own plugins or themes. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+            'callToAction' => 'Get help'
         );
 
         return $menu;
