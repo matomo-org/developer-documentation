d46acbc15c26ca82f0a12905548806bb09c28b7f
Thomas Steur <thomas.steur@gmail.com>
2013-11-07 02:53:56 +0000

    open external links in new tab

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -47,3 +47,11 @@ $('#sidebar').on('hidden.bs.collapse', function () {
         }
     }
 });
+
+$('a').each(function (index, a) {
+    var link = $(a).attr('href');
+
+    if (link && 0 === link.indexOf('http')) {
+        $(a).attr('target', '_blank');
+    }
+});
