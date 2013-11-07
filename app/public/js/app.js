fbfbd3f96d9edcf54eefdcd0271f6e494fea2aa3
Thomas Steur <thomas.steur@gmail.com>
2013-11-07 01:10:20 +0000

    calculate the actual sidebar height (including hidden submenus) and activate affix only if sidebar is smaller than window height. If higher expand all items if screen size is larger than 900

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -3,14 +3,28 @@ function affixSidebar()
 {
     var $sidebar = $('#sidebar');
 
-    if ($sidebar && $sidebar.length && $sidebar.height() <= $(window).height()) {
+    if ($sidebar && $sidebar.length && getActualSidebarHeight() <= $(window).height()) {
         $sidebar.affix({
             offset: {
                 top: 100, bottom: 100
             }
         });
-
+    } else if ($sidebar && $sidebar.length) {
+        $sidebar.addClass('expand')
     }
+}
+
+function getActualSidebarHeight()
+{
+    var copiedElement = $('#sidebar').clone().attr('id', false).css({
+        visibility: 'hidden', display: 'block', position: 'absolute'
+    });
+
+    $('body').append(copiedElement);
+    var height = copiedElement.height();
+    copiedElement.remove();
+
+    return height;
 }
 
 affixSidebar();
