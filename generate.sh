62195c6f721043a3217c1cd2c1e1ad1137c5ed6c
Thomas Steur <tsteur@users.noreply.github.com>
2014-12-19 18:49:30 +1300

    this should fix a doc generation error in case master contains a submodule that is not contained in latest (beta) release

--- generate.sh
+++ generate.sh
@@ -9,7 +9,9 @@ rm -rf docs/generated
 mkdir docs/generated
 cd piwik
 git reset --hard
+git submodule foreach --recursive git reset --hard
 git clean -f -d
+git submodule foreach git clean -f
 git fetch
 git checkout master
 git pull origin master
