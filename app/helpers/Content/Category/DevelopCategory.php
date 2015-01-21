ec2c5b0e0e523ce5b9d6bde41c6419622c5f110b
Matthieu Napoli <matthieu@mnapoli.fr>
2015-01-21 16:09:49 +1300

    Split "Archiving and data" category

--- app/helpers/Content/Category/DevelopCategory.php
+++ app/helpers/Content/Category/DevelopCategory.php
@@ -48,8 +48,10 @@ class DevelopCategory extends Category
                 new Guide('piwiks-reporting-api'),
             ]),
             new Guide('piwik-on-the-command-line'),
-            new EmptySubCategory('Archiving and data', [
+            new EmptySubCategory('Analytics Data', [
                 new Guide('all-about-analytics-data'),
+            ]),
+            new EmptySubCategory('Database', [
                 new Guide('persistence-and-the-mysql-backend'),
                 new Guide('extending-database'),
             ]),
