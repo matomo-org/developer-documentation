f732a02306d2894eb48b358940af8b72d9c65081
Thomas Steur <thomas.steur@gmail.com>
2016-08-11 05:39:30 +0000

    generated directory is now in subdirectory

--- generateAndPush.sh
+++ generateAndPush.sh
@@ -6,7 +6,7 @@ cd piwik
 git checkout master
 git pull origin master
 cd ..
-git checkout docs/generated
+git checkout docs/*/generated
 git pull
 ./generate.sh
 GENERATION_SUCCESS=$?
@@ -14,7 +14,7 @@ if [ $GENERATION_SUCCESS -ne 0 ]; then
   echo -e "Failed to generate documentation" 1>&2;
   exit 1;
 fi
-git add docs/generated
-git rm $(git ls-files --deleted docs/generated)
+git add docs/*/generated
+git rm $(git ls-files --deleted docs/*/generated)
 git commit -m 'updated plugins API documentation'
 git push
