12c447381e95bc7088d5abfd0c2e3467cec4ba76
Lukas Winkler <l.winkler23@mailbox.org>
2017-10-24 07:53:46 +0200

    reorganize search

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -24,42 +24,36 @@ $('.documentation table').addClass('table table-striped table-bordered');
 
 var quickSearchData = null;
 
-var $quickSearchTypeahead = $('#quick-search-typeahead>input');
-$quickSearchTypeahead.typeahead({
-    source: function (query, process) {
-        if (quickSearchData) {
-            process(quickSearchData.names);
-        } else {
-            var url = $quickSearchTypeahead.attr('data-action');
+var $quickSearchTypeahead = $('#quick-search-typeahead').find('>input');
+    $quickSearchTypeahead.on("focus", function () {
+        var url = $quickSearchTypeahead.attr('data-action');
+        $.get(url, {}, function (quickSearchData) {
+            $quickSearchTypeahead.typeahead({
+                source: quickSearchData.names,
+                items: 'all',
+                updater: function (item) {
+                    // get text to display in quick search box
+                    var displayText = item;
+                    console.log(item);
 
-            $.get(url, {}, function (data) {
-                quickSearchData = JSON.parse(data);
-                process(quickSearchData.names);
+                    var trailingEmLoc = displayText.indexOf('<em>');
+                    if (trailingEmLoc !== -1) {
+                        displayText = displayText.substring(0, trailingEmLoc);
+                    }
+
+                    // Track the search
+                    _paq.push(['trackSiteSearch', displayText, false, false]);
+
+                    // get URL to go to
+                    var itemIndex = quickSearchData.names.indexOf(item);
+                    if (itemIndex !== -1) {
+                        window.location.href = quickSearchData.urls[itemIndex];
+                    }
+
+                    // return display text
+                    return $.trim(displayText);
+                }
             });
-        }
-    },
-    items: "all",
-    updater: function (item) {
-        // get text to display in quick search box
-        var displayText = item;
-
-        var trailingEmLoc = displayText.indexOf('<em>');
-        if (trailingEmLoc != -1) {
-            displayText = displayText.substring(0, trailingEmLoc);
-        }
-
-        // Track the search
-        _paq.push(['trackSiteSearch', displayText, false, false]);
-
-        // get URL to go to
-        var itemIndex = quickSearchData.names.indexOf(item);
-        if (itemIndex != -1) {
-            window.location.href = quickSearchData.urls[itemIndex];
-        }
-
-        // return display text
-        return $.trim(displayText);
-    }
-});
-
+        });
+    });
 });
