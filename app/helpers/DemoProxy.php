1033c41650ebb2aa3fbfde6f266e80ce4def5c63
Lachlan Reynolds <lachlan.reynolds@innocraft.com>
2026-03-31 17:31:59 +1300

    Added demo proxy so requests to demo.matomo.cloud are now same origin

---
+++ app/helpers/DemoProxy.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace helpers;
+
+/**
+ * Proxy for demo.matomo.cloud, allows us to make single origin requests by doing them through this class
+ */
+class DemoProxy
+{
+
+    public const MATOMO_SWAGGER_PROXY_TARGET = 'https://demo.matomo.cloud';
+
+    /**
+     * Fetches a demo API GET response and returns the body and status code.
+     *
+     * @param string $url Demo API URL to request.
+     * @param string $authorizationHeader Optional Authorization header value to forward.
+     * @return array{body: string, statusCode: int}
+     */
+    public static function get(string $url, string $authorizationHeader = ''): array
+    {
+        $context = self::createContext($authorizationHeader);
+        $body = self::fetchBody($url, $context);
+        $statusCode = self::parseStatusCode($http_response_header ?? []);
+
+        return [
+            'body' => $body,
+            'statusCode' => $statusCode,
+        ];
+    }
+
+    private static function createContext(string $authorizationHeader)
+    {
+        return stream_context_create([
+            'http' => [
+                'method' => 'GET',
+                'header' => self::buildHeaders($authorizationHeader),
+                'ignore_errors' => true,
+                'timeout' => 30,
+            ],
+        ]);
+    }
+
+    private static function buildHeaders(string $authorizationHeader): string
+    {
+        if ($authorizationHeader === '') {
+            return '';
+        }
+
+        return 'Authorization: ' . $authorizationHeader;
+    }
+
+    private static function fetchBody(string $url, $context): string
+    {
+        $body = @file_get_contents($url, false, $context);
+        if ($body === false) {
+            throw new \RuntimeException('Could not proxy HTTP request');
+        }
+
+        return $body;
+    }
+
+    private static function parseStatusCode(array $responseHeaders): int
+    {
+        $statusLine = $responseHeaders[0] ?? '';
+
+        if (preg_match('#^HTTP/\S+\s+(\d{3})#', $statusLine, $matches)) {
+            return (int) $matches[1];
+        }
+
+        return 200;
+    }
+}
